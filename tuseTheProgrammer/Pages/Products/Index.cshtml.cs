7ec82b3f82078e2dc6c6db02f87394aa555dfe56
Dad  S Wonkulah Jr <dadwonkulahjr96@gmail.com>
2020-09-01 11:06:54 +0000

    Add project files.

---
+++ tuseTheProgrammer/Pages/Products/Index.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace tuseTheProgrammer.Pages.Products
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
